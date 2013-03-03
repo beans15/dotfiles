40b8b8f8a7d70827574094b2e7cbcac5899d15bc
beans15 <beans15@dummy.com>
2013-03-03 23:59:43 +0900

    フォルダをコピーする場合は/Eオプションをつける？

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 REM vimの設定ファイルだけコピー
 XCOPY /H .vimrc %USERPROFILE%\_vimrc
 XCOPY /H .vimrc.plugin %USERPROFILE%\.vimrc.plugin
-XCOPY /H .vim %USERPROFILE%\vimfiles
+XCOPY /H /E .vim %USERPROFILE%\vimfiles
