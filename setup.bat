6a82006c49b7ab4ba1e5d308108c83bcf84f81b8
beans15 <beans15@dummy.com>
2013-03-04 01:01:32 +0900

    フォルダを自動的に作ってくれないようなので作るように変更

--- setup.bat
+++ setup.bat
@@ -4,5 +4,8 @@ XCOPY /H .vimrc.plugin %USERPROFILE%\.vimrc.plugin
 XCOPY /H /E .vim %USERPROFILE%\.vim
 
 IF NOT EXIST %USERPROFILE%\.vim\bundle\neobundle.vim (
+    IF NOT EXIST %USERPROFILE%\.vim\bundle (
+        MD %USERPROFILE%\.vim\bundle
+    )
     git clone git://github.com/Shougo/neobundle.vim.git %USERPROFILE%\.vim\bundle\neobundle.vim
 )
