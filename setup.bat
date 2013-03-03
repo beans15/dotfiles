6febefa2058137161efe4232880ea8d1fe48146a
beans15 <beans15@dummy.com>
2013-03-04 00:48:01 +0900

    setupスクリプトでneobundle.vimをcloneするように設定

--- setup.bat
+++ setup.bat
@@ -2,3 +2,7 @@ REM vimの設定ファイルだけコピー
 XCOPY /H .vimrc %USERPROFILE%\_vimrc
 XCOPY /H .vimrc.plugin %USERPROFILE%\.vimrc.plugin
 XCOPY /H /E .vim %USERPROFILE%\.vim
+
+IF NOT EXIST %USERPROFILE%\.vim\bundle\neobundle.vim (
+    git clone git://github.com/Shougo/neobundle.vim.git
+)
