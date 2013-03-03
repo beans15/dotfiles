8aa38f80b98e548401648b9e348cadaa80390d8e
beans15 <beans15@dummy.com>
2013-03-04 00:59:09 +0900

    neobundle.vimのクローン先を指定していなかったのを修正

--- setup.bat
+++ setup.bat
@@ -4,5 +4,5 @@ XCOPY /H .vimrc.plugin %USERPROFILE%\.vimrc.plugin
 XCOPY /H /E .vim %USERPROFILE%\.vim
 
 IF NOT EXIST %USERPROFILE%\.vim\bundle\neobundle.vim (
-    git clone git://github.com/Shougo/neobundle.vim.git
+    git clone git://github.com/Shougo/neobundle.vim.git %USERPROFILE%\.vim\bundle\neobundle.vim
 )
